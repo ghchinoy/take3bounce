4e13460943208f36acd9bff989ad17463cd9420a
Hussain Chinoy <ghchinoy@google.com>
2026-04-08 08:18:57 -0600

    fix: configure vite to compile audio-tags.html as a multi-page app entry point

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,14 @@ export default defineConfig({
       '/api': {
         target: 'http://127.0.0.1:8080',
         changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sandbox: resolve(__dirname, 'audio-tags.html')
       }
     }
   }
